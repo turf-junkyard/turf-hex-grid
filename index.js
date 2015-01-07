4ad14b8c88d95f5e3f3b81b3688f0d8f90b8d657
Tom MacWright <tom@macwright.org>
2015-01-07 14:38:23 -0500

    Remove callback

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ function hexagon(center, radius) {
   return polygon([vertices]);
 }
 
-function hexgrid(bbox, radius, done) {
+function hexgrid(bbox, radius) {
   var xmin = bbox[0];
   var ymin = bbox[1];
   var xmax = bbox[2];
@@ -54,7 +54,7 @@ function hexgrid(bbox, radius, done) {
 
   var hex_width = radius * 2;
   var hex_height = Math.sqrt(3)/2 * hex_width;
-  
+
   var box_width = xmax - xmin;
   var box_height = ymax - ymin;
 
@@ -66,7 +66,7 @@ function hexgrid(bbox, radius, done) {
   if (Math.round(x_span) === x_count) {
     x_count++;
   }
-  
+
   var x_adjust = ((x_count * x_interval - radius/2) - box_width)/2 - radius/2;
 
   var y_count = Math.ceil(box_height / hex_height);
@@ -77,10 +77,10 @@ function hexgrid(bbox, radius, done) {
   if (hasOffsetY) {
     y_adjust -= hex_height/4;
   }
-  
+
   for (var x = 0; x < x_count; x++) {
     for (var y = 0; y <= y_count; y++) {
-    
+
       var isOdd = x % 2 === 1;
       if (y === 0 && isOdd) {
         continue;
@@ -92,7 +92,7 @@ function hexgrid(bbox, radius, done) {
 
       var center_x = x * x_interval + xmin - x_adjust;
       var center_y = y * y_interval + ymin + y_adjust;
-      
+
       if (isOdd) {
         center_y -= hex_height/2;
       }
@@ -100,9 +100,6 @@ function hexgrid(bbox, radius, done) {
       fc.features.push(hexagon([center_x, center_y], radius));
     }
   }
-
-  done = done || function () {};
-  done(null, fc);
 
   return fc;
 }
