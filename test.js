6b53173698b831cbce3a1459941d793e32ed3779
Tim Channell <tim.channell@gmail.com>
2016-02-12 13:37:17 -0500

    eslint

--- test.js
+++ test.js
@@ -1,27 +1,28 @@
 var test = require('tape');
 var grid = require('./');
 var fs = require('fs');
+var path = require('path');
 var bboxPolygon = require('turf-bbox-polygon');
 
 test('hex-grid', function (t) {
   var bbox1 = [
-        -96.6357421875,
-        31.12819929911196,
-        -84.9462890625,
-        40.58058466412764
-      ];
+    -96.6357421875,
+    31.12819929911196,
+    -84.9462890625,
+    40.58058466412764
+  ];
   var bbox2 = [
-          -81.650390625,
-          24.926294766395593,
-          -79.8486328125,
-          26.43122806450644
-        ];
+    -81.650390625,
+    24.926294766395593,
+    -79.8486328125,
+    26.43122806450644
+  ];
   var bbox3 = [
-        -77.3876953125,
-        38.71980474264239,
-        -76.9482421875,
-        39.027718840211605
-      ];
+    -77.3876953125,
+    38.71980474264239,
+    -76.9482421875,
+    39.027718840211605
+  ];
   var bbox4 = [
     63.6328125,
     11.867350911459308,
@@ -40,27 +41,27 @@ test('hex-grid', function (t) {
   t.ok(grid4.features.length, '50km grid');
 
   t.equal(
-      grid(
-        [-96.6357421875,31.12819929911196,-84.9462890625,40.58058466412764], 
-        100, 'miles')
-      .features.length,
-      85
-    );
+    grid(
+      [-96.6357421875, 31.12819929911196, -84.9462890625, 40.58058466412764],
+      100, 'miles')
+    .features.length,
+    85
+  );
 
   grid1.features.push(referencePoly(bbox1));
   grid2.features.push(referencePoly(bbox2));
   grid3.features.push(referencePoly(bbox3));
   grid4.features.push(referencePoly(bbox4));
 
-  fs.writeFileSync(__dirname+'/fixtures/out/grid1.geojson', JSON.stringify(grid1,null,2));
-  fs.writeFileSync(__dirname+'/fixtures/out/grid2.geojson', JSON.stringify(grid2,null,2));
-  fs.writeFileSync(__dirname+'/fixtures/out/grid3.geojson', JSON.stringify(grid3,null,2));
-  fs.writeFileSync(__dirname+'/fixtures/out/grid4.geojson', JSON.stringify(grid4,null,2));
+  fs.writeFileSync(path.join(__dirname, '/fixtures/out/grid1.geojson'), JSON.stringify(grid1, null, 2));
+  fs.writeFileSync(path.join(__dirname, '/fixtures/out/grid2.geojson'), JSON.stringify(grid2, null, 2));
+  fs.writeFileSync(path.join(__dirname, '/fixtures/out/grid3.geojson'), JSON.stringify(grid3, null, 2));
+  fs.writeFileSync(path.join(__dirname, '/fixtures/out/grid4.geojson'), JSON.stringify(grid4, null, 2));
 
   t.end();
 });
 
-function referencePoly (bbox) {
+function referencePoly(bbox) {
   var poly = bboxPolygon(bbox);
   poly.properties = {
     'fill-opacity': 0,
